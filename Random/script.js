93327da26f9f225817bb80eb2e33af4c81a3ef07
lolbraid <lolbraid@gmail.com>
2022-03-23 18:07:48 +0300

    Part 1

--- Random/script.js
+++ Random/script.js
@@ -1,3 +1,22 @@
 // project أنشئ كائن
-
+const project = {
+  type: "No Thing",
+  users: 1,
+  biography() {
+    return "جملة تعريفية عن المشروع";
+  },
+};
 // randomProperty أنشئ دالة
+(function randomProperty() {
+  let num = Math.floor(Math.random() * 4);
+  console.log(num);
+  if (num == 1) {
+    console.log(project.type);
+  } else if (num == 2) {
+    console.log(project.users);
+  } else if (num == 3){
+    console.log(project.biography());
+  } else {
+      randomProperty()
+  }
+})();
